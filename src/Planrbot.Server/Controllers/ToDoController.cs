7ddc2abf4b2d7a491e0a68075c5861cc1c1df34a
kg73 <kevin@gysberg.me>
2023-02-28 03:02:57 -0600

    Added todolist controller and changed weekview to pull data from it

---
+++ src/Planrbot.Server/Controllers/ToDoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web.Resource;
+
+using Planrbot.Models;
+using Planrbot.Server.Data;
+
+namespace Planrbot.Web.Server.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class ToDoController : ControllerBase
+{
+
+	private readonly ILogger<WeatherForecastController> _logger;
+	private readonly MainDbContext _db;
+
+	public ToDoController(ILogger<WeatherForecastController> logger, MainDbContext db)
+	{
+		_logger = logger;
+		_db = db;
+	}
+
+	[HttpGet]
+	public async Task<IEnumerable<ToDoItem>> GetByWeek(DateTime date = default)
+	{
+		if (date == default)
+		{
+			date = DateTime.Today;
+		}
+
+		var daysAfterSunday = (int)date.DayOfWeek * -1;
+
+		var firstSunday = date.AddDays(daysAfterSunday);
+
+		var items = await _db.ToDoItems.Where(t => t.Date >= firstSunday && t.Date <= firstSunday.AddDays(6)).OrderBy(t => t.Date).ToListAsync();
+
+		return items;
+	}
+
+	[HttpGet("{id}")]
+	public Task<ToDoItem> GetById(Guid id)
+	{
+		return _db.ToDoItems.SingleAsync(t => t.Id == id);
+	}
+
+	[HttpPost]
+	public async Task SaveById(ToDoItem item)
+	{
+		_db.Update(item);
+		await _db.SaveChangesAsync();
+	}
+}
