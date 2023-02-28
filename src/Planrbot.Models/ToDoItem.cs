cc8365f9f3052dd85e490c15a27c9390bf4661d8
kg73 <kevin@gysberg.me>
2023-02-27 23:37:00 -0600

    .

--- src/Planrbot.Models/ToDoItem.cs
+++ src/Planrbot.Models/ToDoItem.cs
@@ -1,7 +1,7 @@
 ﻿namespace Planrbot.Models;
 public class ToDoItem
 {
-	public string Id { get; set; }
+	public string Id { get; set; } = Guid.NewGuid().ToString();
 	public string Description { get; set; }
 	public bool IsComplete { get; set; }
 }
