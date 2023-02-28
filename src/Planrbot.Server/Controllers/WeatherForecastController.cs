a122403dccfa758a96ff6bdc7dbe53ca9a2ed61b
kg73 <kevin@gysberg.me>
2023-02-28 02:38:12 -0600

    changed path of demo controller

--- src/Planrbot.Server/Controllers/WeatherForecastController.cs
+++ src/Planrbot.Server/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using Planrbot.Models;
 namespace Planrbot.Web.Server.Controllers;
 [Authorize]
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
 public class WeatherForecastController : ControllerBase
 {
@@ -33,47 +33,5 @@ public class WeatherForecastController : ControllerBase
 			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
 		})
 		.ToArray();
-	}
-}
-
-
-[Authorize]
-[ApiController]
-[Route("[controller]")]
-[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
-public class ToDoController : ControllerBase
-{
-	private static readonly string[] Summaries = new[]
-	{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
-
-	private readonly ILogger<WeatherForecastController> _logger;
-
-	public ToDoController(ILogger<WeatherForecastController> logger)
-	{
-		_logger = logger;
-	}
-
-	[HttpGet]
-	public IEnumerable<WeatherForecast> GetByWeek()
-	{
-		return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-		{
-			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-			TemperatureC = Random.Shared.Next(-20, 55),
-			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-		})
-		.ToArray();
-	}
-
-	[HttpGet]
-	public ToDoItem GetById(Guid id)
-	{
-		return new ToDoItem()
-		{
-			Description = "Test",
-			IsComplete = false
-		};
 	}
 }
